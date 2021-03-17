45595604770bfac805901ddd26c0df2dc3ca8d11
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2021-03-17 10:16:07 +0530

    UI Done

--- InterfaceExample/Form1.Designer.cs
+++ InterfaceExample/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace InterfaceExample
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -38,16 +40,36 @@ namespace InterfaceExample
             this.comboBox1.Items.AddRange(new object[] {
             "Mp3 CD",
             "Video CD"});
-            this.comboBox1.Location = new System.Drawing.Point(157, 74);
+            this.comboBox1.Location = new System.Drawing.Point(67, 64);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(192, 21);
             this.comboBox1.TabIndex = 0;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(184, 107);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(67, 107);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 247);
+            this.ClientSize = new System.Drawing.Size(341, 198);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Video Player";
@@ -58,6 +80,8 @@ namespace InterfaceExample
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
